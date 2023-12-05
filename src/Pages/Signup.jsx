745db0b50a79145730a04809c9d1c7b30aa62bf8
Luiz <seu-email@example.com>
2023-12-05 11:22:03 -0300

    final

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import './CSS/Signup.css'
-import { Link } from 'react-router-dom'
+import './CSS/Signup.css';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   const initialValues = {
@@ -17,7 +17,7 @@ const Signup = () => {
     password: Yup.string().min(8).required('Password is required'),
   });
 
-  const onSubmit = async (values) => {
+  const onSubmit = async (values, { setSubmitting, setErrors }) => {
     try {
       const response = await fetch('https://152.67.40.201:4406/register', {
         method: 'POST',
@@ -32,31 +32,35 @@ const Signup = () => {
       } else {
         const errorData = await response.json(); // Se o back-end enviar detalhes de erro em JSON
         console.error('Failed to register user:', errorData);
+        setErrors(errorData); // Define os erros no formulário para exibir mensagens de erro ao usuário
       }
     } catch (error) {
       console.error('Error during registration:', error);
+    } finally {
+      setSubmitting(false);
     }
   };
-
 
   return (
     <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
       <Form>
-        <div className='signup'>
+        <div className="signup">
           <div className="signup-container">
             <h1>Sign Up</h1>
             <div className="signup-fields">
-              <Field type="text" name="name" placeholder='Your Name' />
-              <ErrorMessage name="name" component="div" className="error-message" style={{color: 'red', display: 'flex', justifyContent: 'center'}} />
-              <Field type="email" name="email" placeholder='Email Address' />
-              <ErrorMessage name="email" component="div" className="error-message" style={{color: 'red', display: 'flex', justifyContent: 'center'}} />
-              <Field type="password" name="password" placeholder='Password' />
-              <ErrorMessage name="password" component="div" className="error-message" style={{color: 'red', display: 'flex', justifyContent: 'center'}}/>
+              <Field type="text" name="name" placeholder="Your Name" />
+              <ErrorMessage name="name" component="div" className="error-message" style={{ color: 'red', display: 'flex', justifyContent: 'center' }} />
+              <Field type="email" name="email" placeholder="Email Address" />
+              <ErrorMessage name="email" component="div" className="error-message" style={{ color: 'red', display: 'flex', justifyContent: 'center' }} />
+              <Field type="password" name="password" placeholder="Password" />
+              <ErrorMessage name="password" component="div" className="error-message" style={{ color: 'red', display: 'flex', justifyContent: 'center' }} />
             </div>
             <button type="submit">Sign Up</button>
             <p className="signup-login">
               Already have an account?
-              <Link to='../../login' style={{textDecoration: 'none'}}><span>Login here</span></Link>
+              <Link to="../../login" style={{ textDecoration: 'none' }}>
+                <span>Login here</span>
+              </Link>
             </p>
           </div>
         </div>
